3c1f2163bd6155a9c3adb5a5eb5c359ad86bf672
Justin Hoyt <justinhoyt24@gmail.com>
2019-07-11 12:53:39 -0400

    Delete slack_darkmode.js

--- slack_darkmode.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
- $.ajax({
-   url: 'https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css',
-   success: function(css) {
-     $("<style></style>").appendTo('head').html(css);
-   }
- });
-});
