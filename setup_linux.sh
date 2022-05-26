ddbff95e641bdb3c09f46912200b1cb5a1dacc86
Justin Hoyt <justinhoyt@google.com>
2022-05-26 01:53:06 -0700

    Add nix check before installing

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-sh <(curl -L https://nixos.org/nix/install) --daemon
+if ! command -v nix &> /dev/null; then
+    sh <(curl -L https://nixos.org/nix/install) --daemon
+fi
+
 echo "${HOME}/.nix-profile/bin/zsh" | sudo -S tee -a /etc/shells
 su - $USER -c 'bash ~/.dotfiles/setup_common.sh'
