8290f472cf92b9f63ac0cdc740877ef204223a02
Justin Hoyt <justinhoyt24@gmail.com>
2023-03-17 19:06:24 -0700

    Add commands to update nvim's background

--- fish/config.fish
+++ fish/config.fish
@@ -9,6 +9,12 @@ if status is-interactive
     abbr b "bash -c"
 
     alias v nvim
+    function vdark
+        sed -i "s/vim.o.background='light'/vim.o.background='dark'/" ~/.config/nvim/init.lua
+    end
+    function vlight
+        sed -i "s/vim.o.background='dark'/vim.o.background='light'/" ~/.config/nvim/init.lua
+    end
 
     set -g fish_escape_delay_ms 10
 
