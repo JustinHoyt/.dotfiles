bce5bbb7d651d87e55b95553054031e45803ee91
Justin Hoyt <justinhoyt@google.com>
2023-08-19 13:49:15 -0700

    Update perl abbreviations

--- fish/config.fish
+++ fish/config.fish
@@ -17,6 +17,8 @@ if status is-interactive
     abbr caffeine 'caffeinate -imdsu'
     abbr b --set-cursor "bash -c '%'"
     abbr apti 'sudo apt install -y'
+    # Sync histories with other open fish shells
+    abbr hm 'history merge'
     # Installation command for pkgsrc
     abbr pkgi "sudo (which bmake) install clean clean-depends"
     abbr --set-cursor whi --position anywhere "$(string join \n -- 'while read line' '% $line' 'end')"
@@ -26,26 +28,29 @@ if status is-interactive
     abbr --set-cursor ent --position anywhere "| entr -c -r"
 
     # Perl abbreviations
-    abbr --set-cursor pe --position anywhere "perl -My -E 'say %'"
-    abbr --set-cursor pp --position anywhere "perl -My -pE '%'"
-    abbr --set-cursor pn --position anywhere "perl -My -nE '%'"
-    abbr --set-cursor pnp --position anywhere "perl -My -nE 'print if m#%#g'"
+    abbr --set-cursor pe --position anywhere "perl -E 'say %'"
+    abbr --set-cursor pp --position anywhere "perl -pE '%'"
+    abbr --set-cursor pn --position anywhere "perl -nE '%'"
+    abbr --set-cursor pnp --position anywhere "perl -nE 'print if m#%#g'"
+    abbr --set-cursor pnu --position anywhere "perl -nE 'print unless m#%#g'"
     # Find files
     abbr --set-cursor pef "perl -My -E 'find(sub { say if m#%# }, \".\")'"
     # Show before and after of a Substitute
-    abbr --set-cursor pa --position anywhere "perl -My -aE 'say %'"
-    abbr --set-cursor pfc --position anywhere "perl -My -F',' -E 'say % @F'"
+    abbr --set-cursor pa --position anywhere "perl -aE 'say %'"
+    # Field separator as newline, Record separator as a comma
+    abbr --set-cursor pfc --position anywhere "perl -F',' -E 'say % @F'"
     # Field separator as newline, Record separator as 2 newlines
-    abbr --set-cursor pfn --position anywhere "perl -My -00 -F'\n' -E 'say % @F'"
-    abbr --set-cursor pft --position anywhere "perl -My -F'\t' -E 'say % @F'"
+    abbr --set-cursor pfn --position anywhere "perl -00 -F'\n' -E 'say % @F'"
+    # Field separator as newline, Record separator as a tab
+    abbr --set-cursor pft --position anywhere "perl -F'\t' -E 'say % @F'"
     # Record separator as space
-    abbr --set-cursor pos --position anywhere "perl -My -040 -nE 'print %'"
+    abbr --set-cursor p04 --position anywhere "perl -040 -nE 'print %'"
     # Record separator as 2 newlines
-    abbr --set-cursor poo --position anywhere "perl -My -00 -nE 'print %'"
+    abbr --set-cursor p00 --position anywhere "perl -00 -nE 'print %'"
     # Slurp whole file into one record
-    abbr --set-cursor po7 --position anywhere "perl -My -0777 -nE 'print %'"
+    abbr --set-cursor p07 --position anywhere "perl -0777 -nE 'print %'"
     # Find and Replace
-    abbr --set-cursor pr --position anywhere "perl -My -i -pE"
+    abbr --set-cursor pr --position anywhere "perl -i -pE 's#%#&#g'"
     # Find and Replace preview
     abbr --set-cursor prv --position anywhere "ppr 's#%#\$&#g'"
 
