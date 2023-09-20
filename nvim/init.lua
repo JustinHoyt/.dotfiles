6c9cbc2c7aa4a04e06475b19fc091a80d864e9f1
Justin Hoyt <justinhoyt@google.com>
2023-09-19 23:11:43 -0700

    Add relative filename in lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -90,6 +90,15 @@ local plugins = {
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'%f'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+      },
+
     },
   },
 
