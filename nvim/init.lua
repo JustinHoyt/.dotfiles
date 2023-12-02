995f77f6a471f43c287d260eee2edbf76c9dc278
Justin Hoyt <justinhoyt@google.com>
2023-12-02 01:29:03 -0800

    Add telescope undo and text-case.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -282,6 +282,40 @@ local plugins = {
     end,
   },
 
+  {
+    "debugloop/telescope-undo.nvim",
+    keys = {
+      { -- lazy style key map
+        "<leader>su",
+        "<cmd>Telescope undo<cr>",
+        desc = "undo history",
+      },
+    },
+    opts = {
+      -- don't use `defaults = { }` here, do this in the main telescope spec
+      extensions = {
+        undo = {
+          -- telescope-undo.nvim config, see below
+        },
+        -- no other extensions here, they can have their own spec too
+      },
+    },
+    config = function(_, opts)
+      -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+      -- configs for us. We won't use data, as everything is in it's own namespace (telescope
+      -- defaults, as well as each extension).
+      require("telescope").setup(opts)
+      require("telescope").load_extension("undo")
+    end,
+  },
+
+  {
+    "johmsalas/text-case.nvim",
+    config = function()
+      require('textcase').setup {}
+    end
+  },
+
   'ray-x/guihua.lua', -- recommended if need floating window support
   'ray-x/go.nvim',
   'nickeb96/fish.vim',
@@ -688,8 +722,8 @@ vim.keymap.set('t', '<esc><esc>', '<C-\\><C-N>', {noremap = true})
 -- Regex substitute with very magic mode shortcut
 vim.keymap.set('n', 'gs', ':%s#\\v#&#g<left><left><left><left>', {noremap = true})
 vim.keymap.set('v', 'gs', ':s#\\v#&#g<left><left><left><left>', {noremap = true})
-vim.keymap.set('n', 'gS', ':%S###g<left><left><left>', {noremap = true})
-vim.keymap.set('v', 'gS', ':S###g<left><left><left>', {noremap = true})
+vim.keymap.set('n', 'gS', ':Subs///g<left><left><left>', {noremap = true})
+vim.keymap.set('v', 'gS', ':Subs///g<left><left><left>', {noremap = true})
 vim.keymap.set('n', 'g/', '/\\v', {noremap = true})
 
 -- Paste all regex line matches to the current line
