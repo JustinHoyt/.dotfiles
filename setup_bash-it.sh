9f8e7f60faa98c6678b793aee44b4f35837ca130
justinhoyt <justinhoyt@gmail.com>
2024-02-05 07:40:27 +0000

    add tpm plugin install and dotfile install to ./setup_dotfiles

--- setup_bash-it.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh --silent --no-modify-config
-
-source "$BASH_IT"/bash_it.sh
--- setup_ble.sh
+++
@@ -1,2 +0,0 @@
-git clone --recursive https://github.com/akinomyoga/ble.sh.git ~/.ble.sh
-(cd ~/ && make -C .ble.sh install PREFIX=~/.local)
