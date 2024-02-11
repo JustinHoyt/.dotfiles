2e54f4970dffbaa67554fd2161fe7ff17a9ebd0e
RapidSeedbox.com User <user@rapidseedbox.com>
2024-02-11 11:09:08 +0000

    Update undent-blankline

--- nvim/init.lua
+++ nvim/init.lua
@@ -76,16 +76,7 @@ local plugins = {
 		end,
 	},
 
-	{
-		-- Add indentation guides even on blank lines
-		"lukas-reineke/indent-blankline.nvim",
-		-- Enable `lukas-reineke/indent-blankline.nvim`
-		-- See `:help indent_blankline.txt`
-		opts = {
-			char = "┊",
-			show_trailing_blankline_indent = false,
-		},
-	},
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
 	-- "gc" to comment visual regions/lines
 	{ "numToStr/Comment.nvim", opts = {} },
