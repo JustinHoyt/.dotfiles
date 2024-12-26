c396abf6d605848b41bda78a28582af1195fa8d3
justinhoyt <justinhoyt@gmail.com>
2024-12-26 08:28:54 +0000

    Fix small string/object param type error in LSP mapping

--- nvim/init.lua
+++ nvim/init.lua
@@ -724,35 +724,53 @@ local on_attach = function(_, bufnr)
 	--
 	-- In this case, we create a function that lets us more easily define mappings specific
 	-- for LSP related items. It sets the mode, buffer and description for us each time.
-	local nmap = function(keys, func, desc)
-		if desc then
-			desc = "LSP: " .. desc
-		end
 
-		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-	end
+	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "[R]e[n]ame" })
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "[C]ode [A]ction" })
 
-	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+	vim.keymap.set(
+		"n",
+		"<leader>sr",
+		require("telescope.builtin").lsp_references,
+		{ desc = "[G]oto [R]eferences", noremap = true }
+	)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "[G]oto [D]efinition" })
+	vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { buffer = bufnr, desc = "[G]oto [I]mplementation" })
+	vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, { buffer = bufnr, desc = "Type [D]efinition" })
+	vim.keymap.set(
+		"n",
+		"<leader>ds",
+		require("telescope.builtin").lsp_document_symbols,
+		{ buffer = bufnr, desc = "[D]ocument [S]ymbols" }
+	)
+	vim.keymap.set(
+		"n",
+		"<leader>ws",
+		require("telescope.builtin").lsp_dynamic_workspace_symbols,
+		{ buffer = bufnr, desc = "[W]orkspace [S]ymbols" }
+	)
 
-	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-	nmap("<leader>sr", require("telescope.builtin").lsp_references, { desc = "[G]oto [R]eferences", noremap = true })
-	nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-	nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-	nmap("gh", vim.lsp.buf.hover, "Hover Documentation")
-	nmap("<C-s>", vim.lsp.buf.signature_help, "Signature Documentation")
+	vim.keymap.set("n", "gh", vim.lsp.buf.hover, { buffer = bufnr, desc = "Hover Documentation" })
+	vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "Signature Documentation" })
 	vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "Signature Documentation" })
 
 	-- Lesser used LSP functionality
-	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-	nmap("<leader>wl", function()
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "[G]oto [D]eclaration" })
+	vim.keymap.set(
+		"n",
+		"<leader>wa",
+		vim.lsp.buf.add_workspace_folder,
+		{ buffer = bufnr, desc = "[W]orkspace [A]dd Folder" }
+	)
+	vim.keymap.set(
+		"n",
+		"<leader>wr",
+		vim.lsp.buf.remove_workspace_folder,
+		{ buffer = bufnr, desc = "[W]orkspace [R]emove Folder" }
+	)
+	vim.keymap.set("n", "<leader>wl", function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, "[W]orkspace [L]ist Folders")
+	end, { buffer = bufnr, desc = "[W]orkspace [L]ist Folders" })
 
 	-- Create a command `:Format` local to the LSP buffer
 	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
